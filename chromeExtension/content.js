14fbe0c01f405185bfb991507b4e6d4302d9b931
Julian Castro <jcas2tur@gmail.com>
2023-11-08 12:22:18 -0500

    content.js init, translate on text highlight

---
+++ chromeExtension/content.js
@@ -0,0 +1,53 @@
+function translateText(text, srcLang, destLang) {
+  const payload = {
+    text: text,
+    src: srcLang,
+    dest: destLang,
+  };
+
+  fetch("http://127.0.0.1:5000/translate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      replaceSelectedText(data.translatedText);
+    })
+    .catch((error) => console.error("Error translating text:", error));
+}
+
+function replaceSelectedText(translatedText) {
+  const selection = window.getSelection();
+  if (!selection.rangeCount) return;
+
+  const range = selection.getRangeAt(0);
+  const selectedElement =
+    range.commonAncestorContainer.nodeType === 3
+      ? range.commonAncestorContainer.parentNode
+      : range.commonAncestorContainer;
+
+  function replaceTextInNode(node) {
+    if (node.nodeType === 3) {
+      node.textContent = translatedText;
+    } else if (node.nodeType === 1) {
+      Array.from(node.childNodes).forEach(replaceTextInNode);
+    }
+  }
+
+  replaceTextInNode(selectedElement);
+
+  selection.removeAllRanges();
+}
+
+document.addEventListener("mouseup", function (event) {
+  const selectedText = window.getSelection().toString();
+  if (selectedText.length > 0) {
+    const srcLang = "en";
+    const destLang = "es";
+
+    translateText(selectedText, srcLang, destLang);
+  }
+});
